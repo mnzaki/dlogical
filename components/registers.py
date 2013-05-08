3c83da6b651dc6b5698c4b6757e8b14eb7b40eb7
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-08 16:05:34 +0200

    Add 32bit D register component.

---
+++ components/registers.py
@@ -0,0 +1,14 @@
+from component import Component
+from ..simulator import Delta
+
+class DRegister32(Component):
+  delay = 100
+  inputs = {'q': 32}
+  outputs = {'d': 32}
+
+  def __init__(self, **kwargs):
+    super(DRegister, self).__init__(**kwargs)
+
+  def simulate(self):
+    self.d.data = self.q.data.copy()
+    return Delta(self.delay, [self.d])
