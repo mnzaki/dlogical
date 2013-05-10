35b8849fb474de8890dfe2582de249c22d45cb59
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-10 12:37:44 +0200

    Add (untested) Mem component.

---
+++ components/mem.py
@@ -0,0 +1,28 @@
+from ..component import ParametrizedComponent
+import math
+
+class Mem(ParametrizedComponent):
+  delay = 400
+
+  parameters = {'width': 32, 'size': 1024 }
+
+  inputs  = {'addr':     'log_size',
+             'write':    'width',
+             'write_en': 1}
+
+  outputs = {'read':     'width'}
+
+  @classmethod
+  def process_parameters(klass, params):
+    params['log_size'] = int(math.ceil(math.log(params['size'], 2)))
+
+  def __init__(self, mem = None):
+    mem = mem or [0] * self.parameters['size']
+    self.mem = mem
+
+  def simulate(self, ins, outs):
+    if self.write_en.data == 1:
+      self.mem[self.addr.data] = self.write.data
+      outs.read = self.write.data
+    elif 'addr' in ins:
+      outs.read = self.mem[ins.addr]
