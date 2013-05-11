e88339cf4776300af3d5c96cc8b0ccb0b151927e
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-11 13:57:52 +0200

    Fix several syntax errors and typos in the MIPs arch description.

--- mips/arch.py
+++ mips/arch.py
@@ -1,15 +1,15 @@
 from control import *
 from alu import ALU
-from ..components.misc import *
-from ..components.registers import *
-from ..components.mem import Mem
+from mips_sim.components.misc import *
+from mips_sim.components.registers import *
+from mips_sim.components.mem import Mem
 
 # It starts with a counter :')
 pc = DRegister32()
 
 imem = Mem.with_parameters(width = 32, size = 1024)(addr = pc.d[:], write_en = 0)
 
-control = ControlUnit(opcode = imem[31:26])
+control = ControlUnit(opcode = imem.read[31:26])
 alucontrol = ALUControlUnit(aluop = control.aluop[:],
                             funct = imem.read[5:0],
                             opcode = imem.read[31:26])
@@ -19,21 +19,21 @@ write_reg_mux = Mux(in0 = imem.read[20:16], in1 = imem.read[15:11], s = control.
 regs = RegisterFile.with_parameters(num_regs = 32, width = 32)(
         read_reg1 = imem.read[25:21],
         read_reg2 = imem.read[20:16],
-        write_reg = WRITE_REG_MUX.out[:])
+        write_reg = write_reg_mux.out[:])
 
 inst_sign_ext = SignExt(inp = imem.read[15:0])
 
-alusrc_mux = Mux(in0 = regs.read2[:], in1 = inst_sign_ext.out[:], s = control.alusrc)
+alusrc_mux = Mux(in0 = regs.read2[:], in1 = inst_sign_ext.out[:], s = control.alusrc[:])
 
-alu = ALU(alucontrol = control.alucontrol[:], in0 = regs.read1[:], in1 = alusrc_mux.out[:])
+alu = ALU(control = alucontrol.alucontrol[:], in0 = regs.read1[:], in1 = alusrc_mux.out[:])
 
-dmem = Mem.with_parameters(width = 32 size = 8192)(
+dmem = Mem.with_parameters(width = 32, size = 8192)(
         addr = alu.out[:],
         write = regs.read2[:],
         write_en = control.memwrite[:],
         read_en = control.memread[:])
 
-regs_write_data_mux = Mux(in0 = alu.out[:], in1 = dmem.read[:], s = control.memtoreg)
+regs_write_data_mux = Mux(in0 = alu.out[:], in1 = dmem.read[:], s = control.memtoreg[:])
 
 regs.write = regs_write_data_mux.out[:]
 
