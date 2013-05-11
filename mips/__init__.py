f046c660f25dde4c91ce763ca5d4c11d049d4bc6
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-11 18:49:47 +0200

    Auto name components in mips.arch

--- mips/__init__.py
+++ mips/__init__.py
@@ -1,1 +1,8 @@
 __all__ = ['arch', 'asm', 'control', 'alu']
+
+import arch
+from components.component import Component
+
+for k, v in arch.__dict__.iteritems():
+  if isinstance(v, Component):
+    v.name = k
